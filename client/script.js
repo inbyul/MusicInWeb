048c15c0a11ac340c35cb03d07d7cccd8b349c65
inbyul <89333548+inbyul@users.noreply.github.com>
2021-09-08 04:06:07 +0000

    front update

--- client/script.js
+++ client/script.js
@@ -1,1 +1,6 @@
-iframe.src = prompt("웹 주소 입력");
+var link = prompt("웹 주소 입력");
+if (link.startsWith("http://") || link.startsWith("https://")) {
+  iframe.src = link;
+} else {
+  alert("웹주소가 잘못되었습니다!");
+}
